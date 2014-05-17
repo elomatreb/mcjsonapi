446146950b9398f4feea5924305b94f4ce5c28d5
Ole B <elomatreb@googlemail.com>
2014-05-17 04:21:44 +0200

    Added tests for new instances

--- lib/mcjsonapi.rb
+++ lib/mcjsonapi.rb
@@ -2,10 +2,17 @@ require "mcjsonapi/version"
 
 module Mcjsonapi
   class API
+    attr_reader :host, :port
+
     def initialize(options = {})
-      raise "Username and password are needed." if options.empty?
+      # Validation
+      raise "Username and password are needed." if options[:username].nil? && options[:password].nil?
       raise "Password is needed." if options[:password].nil?
       raise "Username is needed." if options[:username].nil?
+
+      # Assigning variables
+      @host = options[:host] || "localhost"
+      @port = options[:port] || 20059
     end
   end
 end
--- spec/mcjsonapi_spec.rb
+++ spec/mcjsonapi_spec.rb
@@ -1,14 +1,12 @@
 require "spec_helper"
 
-describe Mcjsonapi do
-  before do
-    # You need a local Minecraft server running on localhost:20059 with these
-    # JSONAPI credentials to run the tests.
-    @username = "testuser"
-    @password = "password"
-  end
+describe "Mcjsonapi:" do
+  # You need a local Minecraft server running on localhost:20059 with these
+  # JSONAPI credentials to run the tests.
+  let(:username) { "testuser" }
+  let(:password) { "password" }
 
-  describe "instance" do
+  describe "new instance" do
     describe "with invalid information" do
       describe "without any information" do
         it "should raise an error" do
@@ -18,13 +16,47 @@ describe Mcjsonapi do
 
       describe "without password" do
         it "should raise an error" do
-          expect { Mcjsonapi::API.new username: @username }.to raise_error("Password is needed.")
+          expect { Mcjsonapi::API.new username: username }.to raise_error("Password is needed.")
         end
       end
 
       describe "without username" do
-        it "should raise an exception" do
-          expect { Mcjsonapi::API.new password: @password }.to raise_error("Username is needed.")
+        it "should raise an error" do
+          expect { Mcjsonapi::API.new password: password }.to raise_error("Username is needed.")
+        end
+      end
+    end
+
+    describe "with valid information" do
+      describe "with no host given" do
+        let(:api) { Mcjsonapi::API.new username: username, password: password }
+
+        it "should assume localhost as default" do
+          expect(api.host).to eq "localhost"
+        end
+      end
+
+      describe "with host given" do
+        let(:api) { Mcjsonapi::API.new host: "api.example.com", username: username, password: password }
+
+        it "should use the given host" do
+          expect(api.host).to eq "api.example.com"
+        end
+      end
+
+      describe "with no port given" do
+        let(:api) { Mcjsonapi::API.new username: username, password: password }
+
+        it "should assume 20059 as default" do
+          expect(api.port).to eq 20059
+        end
+      end
+
+      describe "with port given" do
+        let(:api) { Mcjsonapi::API.new port: 25565, username: username, password: password }
+
+        it "should use the given port" do
+          expect(api.port).to eq 25565
         end
       end
     end
